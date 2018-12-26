7a11f64d0c5125cf26769e1156230b550e0c6b73
Bo Yang <teboring@google.com>
2018-12-26 14:39:13 -0800

    Install ruby 2.5.1

--- kokoro/release/ruby/linux/ruby/ruby_build_environment.sh
+++ kokoro/release/ruby/linux/ruby/ruby_build_environment.sh
@@ -3,6 +3,7 @@
 set +ex
 [[ -s /etc/profile.d/rvm.sh ]] && . /etc/profile.d/rvm.sh
 set -e  # rvm commands are very verbose
+rvm install ruby-2.5.1
 rvm --default use ruby-2.5.1
 gem install bundler --update
 set -ex
