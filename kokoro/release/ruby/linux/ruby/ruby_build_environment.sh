68d5a420729f3968a1e88b496a9d32081fb6b46b
Bo Yang <teboring@google.com>
2018-12-26 17:12:41 -0800

    Use 2.4.1 as default

--- kokoro/release/ruby/linux/ruby/ruby_build_environment.sh
+++ kokoro/release/ruby/linux/ruby/ruby_build_environment.sh
@@ -3,6 +3,6 @@
 set +ex
 [[ -s /etc/profile.d/rvm.sh ]] && . /etc/profile.d/rvm.sh
 set -e  # rvm commands are very verbose
-rvm --default use ruby-2.4
+rvm --default use ruby-2.4.1
 gem install bundler --update
 set -ex
